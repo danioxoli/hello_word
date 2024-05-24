edf5b12d4c15d07fd46c791c5f5c878bfa72956e
danioxoli <daniele.oxoli@gmail.com>
2024-05-24 14:55:03 +0200

    Add a new code segment.

--- hello.py
+++ hello.py
@@ -1,16 +1,7 @@
 
-import sys
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
 
-if len(sys.argv) > 1:
-    name = sys.argv[1]
-else:
-    print("You are expected to provide your name.")
-    print("You need to type:")
-    print(f"\t python {sys.argv[0]} your_name")
-    sys.exit()
-print(f"My name is {name}.")
-print()
+name = "Julia"
+print_message(name)
 
-print("Hello World!")
-
-print("\t This is my first Git repository on GitHub.")
